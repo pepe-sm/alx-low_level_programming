4b23ad93c8c9382badd845607230489ad980f1db
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-16 09:36:25 -0800

    mahn! betty linter doesnt want errorshad missed a space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,6 @@
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	printf("Size of a char: %i byte(s)\n", sizeof(char));
@@ -12,5 +11,5 @@ int main(void)
 	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %i byte(s)\n", sizeof(float));
-	return(0);
+	return (0);
 }
