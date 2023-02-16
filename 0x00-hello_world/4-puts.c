f8cda7d1d5fcd140ce6c0047455e3f0dde5f1bef
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-16 04:07:03 -0800

    dint use the betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,8 @@
  * main -Entry point
  * return: Always 0(Success)
  */
-
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-          return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
 }
