37a3a399b6549f2e6430262c0e206ef30d1d48a0
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-17 05:40:35 -0800

    alpha in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - print alp in reverse.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char rev;
+
+	for (rev = 'z'; rev >= 'a'; rev--)
+		putchar(rev);
+	putchar ('\n');
+
+	return (0);
+}
