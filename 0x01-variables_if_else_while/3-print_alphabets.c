f19c9750b153d02460285bfc48a95f684137d943
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-17 02:58:43 -0800

    using for loop to output alph in lower and upper case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - prints alpha numeric in Upper & lower case
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= z; alph++)
+		putchar(alph);
+
+	for (alph = 'A'; alph <= Z; alph++)
+		putchar(alph);
+
+		putchar('\n');
+
+		return (0);
+}
