ba76194f1d750522d2dd941cd1e451eef781ff2e
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-21 08:54:04 -0800

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_last_digit - prints last digit
+ * @n: the number to be treated
+ * Return: the value of last digit
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+
+	last = n % 10;
+	if ( last < 0)
+	{
+	last *= -1;
+	}
+	_putchar(last + '0');
+	return (last);
+}
