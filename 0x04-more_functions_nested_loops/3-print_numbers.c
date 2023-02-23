231ff4a24468406fd0b33df732915101762d5bc6
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-23 07:38:50 -0800

    prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_numbers - Prints 0 to 9
+ * Return: The number from 0 to 9
+ */
+
+void prints_numbers(void)
+{
+	int x;
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+}
