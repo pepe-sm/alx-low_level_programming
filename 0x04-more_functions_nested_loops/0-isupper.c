f75c53f54bcb89143d93ea1c93dead459dac215f
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-23 07:03:24 -0800

    cecking for uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _isupper - Check if a leter is upper
+ * @x: the number to be checked
+ *
+ * Return: 1 for upper and 0 for lower case
+ */
+
+int _isupper(int x)
+{
+	if (i >= 65 && i <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
