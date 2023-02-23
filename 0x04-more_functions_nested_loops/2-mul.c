be2218d836ba04b4bfbd43915c97edd31fd8b8c6
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-23 07:29:59 -0800

    returns multiplication of two ints

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * mul - Multiplies two integers
+ * @x: first integer
+ * @y: second integer
+ * Return: product of x amd  y
+ */
+
+int mul(int x, int y)
+{
+	return(x*y);
+}
