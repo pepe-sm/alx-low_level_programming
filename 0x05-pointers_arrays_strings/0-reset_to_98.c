d6deb9b11fad6770d45a97ae4ccccbf11d6f74eb
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-27 05:49:31 -0800

    small error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * rest_to_98 - takes pointer to an int as its parameter
+ * reset_to_98 - takes pointer to an int as its parameter
  * and updates the value it points to 98
  * @n: input
  * Return: n
