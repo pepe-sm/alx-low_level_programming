b60162079675a2a7fe4867325154deb7060eec5a
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-27 03:10:20 -0800

    using pointers to swap value of variable in main

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * rest_to_98 - takes pointer to an int as its parameter
+ * and updates the value it points to 98
+ * @n: input
+ * Return: n
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+the chapter explains pointers, it dives deep to show how pointers can be used to manipulate values, gives also details of how arrays are treated and ow to acces the pointer to each value in the array
