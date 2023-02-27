2592c74e58b4aa24d29596c304b35e1c7c652ee8
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-02-27 04:04:41 -0800

    revesre print a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include"main.h"
+
+/**
+ * print_rev - prints the reverse of a string
+ * @s: string to be reversed
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]0;
+
+	_putchar('\n');
+
+
+
+}
