12f0d81cb0448684f02e8af32d3879ef736fddf3
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-03-02 02:48:55 -0800

    encoding a string

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,17 +6,17 @@
  * Return: Pointer to the encoded string
  */
 
-char leet(char *str)
+char *leet(char *str)
 {
 	int index1 = 0, index2;
-	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'}
+	char leet[8] = {'O', 'L', '?', 'E', 'A', '?', '?', 'T'};
 
 	while (str[index1])
 	{
 		for (index2 = 0; index2 <= 7; index2++)
 		{
 		if (str[index1] == leet[index2] ||
-		str[index1] - 32 == let[index2])
+		str[index1] - 32 == leet[index2])
 			str[index1] = index2 + '0';
 		}
 	index1++;
