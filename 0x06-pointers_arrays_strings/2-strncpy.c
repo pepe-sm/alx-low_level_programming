340ff6f74873701e81c46be2cd45cc335ee3e343
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-03-01 23:22:59 -0800

    copying strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,10 +13,10 @@ char *_strncpy(char *dest, char *src, int n)
 	int src_len = 0, index = 0;
 
 	while (src[index++])
-		scr_len++;
+		src_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-	dest[index] = src[index];
+		dest[index] = src[index];
 
 	for (index = src_len; index < n; index++)
 		dest[index] = '\0';
