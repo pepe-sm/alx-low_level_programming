11d63f60bdf3f3f46d7ec01b2add4c87548e0208
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-03-08 11:34:12 -0800

    string length recursion mode

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include"main.h"
+
+/**
+ * _strlen_recursion - print string length
+ * @s: String to be counted
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s)
+	{
+	len++;
+	len += _strlen_recursion(s + 1);
+	}
+}
