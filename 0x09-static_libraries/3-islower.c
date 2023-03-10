85fff77d5a35d7d423c56105f22047937fe0bbab
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-03-10 06:12:10 -0800

    adding rest of files

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,10 @@
+#include"main.h"
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int _islower(int c)
+{
+	return ( c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,10 @@
+#include"main.h"
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+
+int _putchar(char c)
+{
+	return putchar(c);
+}
