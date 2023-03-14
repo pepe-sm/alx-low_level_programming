26dfd205b25c5bdd94776ce32f678cbbe574eb23
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-03-14 06:02:07 -0700

    string concat, malloc assigns memory

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 
 	if (s1 == NULL)
-		return ("");
+		s1 = "";
 	if (s2 == NULL)
-		return ("");
+		s2 = "";
 
 	while (s1[i])
 	{
@@ -40,7 +40,7 @@ char *str_concat(char *s1, char *s2)
 
 	while (k < l)
 	{
-	if (k <= i) 
+	if (k <= i)
 		s[k] = s1[k];
 	if (k >= i)
 	{
