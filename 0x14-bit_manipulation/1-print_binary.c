fcd9cbe4a8c677e0ecdb3233b409334236e03129
Stephen Mburu Gitau <stephenpepe.sm@gmail.com>
2023-04-06 01:27:54 -0700

    output binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_binary - converts deecimal to binary and prints
+ * @n: number to be converted
+ */
+
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+	print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
