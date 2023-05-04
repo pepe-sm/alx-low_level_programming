4f875cfc4f49196f426a7a405eea2f0668616875
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-05-04 13:24:47 -0700

    task 3- output value of bit at given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * get_bit - return bit value at given index
+ * @index: index to be returned
+ * @n: int
+ * Return: valur of index or -1 on error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int i;
+	if (index > 64)
+	return (-1);
+
+	i = n >> index;
+
+	return (i & 1);
+}
