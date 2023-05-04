41453dbfa0f526715c2874a900ebbcd57e00168c
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-05-04 13:57:29 -0700

    endian

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ * Return: 0 big endian, 1 little endian
+ */
+
+int get_endianness(void)
+{
+	int num = 1;
+	char *i = (char *)&num;
+	
+	if (*i == 1)
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
