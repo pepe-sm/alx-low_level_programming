7fe33a8ab742525bc89aa936c6f5094a40de3346
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-05-08 13:18:10 -0700

    task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads and prints text file to POSIX stdout
+ * @filename: ptr to file name
+ * @letters: no of letters
+ * Return: 0 on error
+*/
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+ssize_t op, rd, wr;
+
+char *buf;
+
+if (!filename)
+return (0);
+
+buff = malloc(sizeof(char) * letters);
+if (buff == NULL)
+return (0);
+
+op = open(filename, O_RDONLY);
+rd = read(o, buf, letters);
+wr = write(STDOUT_FILENO, buf, r);
+
+if (op == -1 || rd == -1 || wr == -1 || wr != rd)
+{
+free(buff);
+return (0);
+}
+
+free(buff);
+close(op);
+
+return (wr);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+in this repo i will handle file io.
