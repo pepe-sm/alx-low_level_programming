42800a5f104508fa2fa942cba46db931b4a83844
Stephen Mburu <stephenpepe.sm@gmail.com>
2023-06-15 11:14:24 -0700

    printing double linked list nodes

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,23 @@
+#include"lists.h"
+
+/**
+ * print_dlistint - print element of a list
+ * @h: node pointer
+ *
+ * Return: number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	int node = 0;
+	
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		node++;
+
+		h = h->next;
+
+	}
+	return (node);
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+this repository covers everything to do with double linked list, from understanding what a double linked list is to adding node at different positions. 
